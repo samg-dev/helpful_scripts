f79a15bbadf508657d409b3d592f98988ec7c3cf
Samuel <sam@prometheusresearch.com>
2022-04-23 12:43:16 +0100

    Create startup.bat

---
+++ startup.bat
@@ -0,0 +1,6 @@
+START "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio\OBS Studio (64bit)"
+START "" "C:\Users\Samuel\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\StreamLabels"
+START "" "E:\Program Files\Chatterino7\chatterino.exe"
+START "" "C:\Users\Samuel\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Opera browser"
+START "" ""
+py D:\MyVods\FileHandler.py
